c3665a1e7a38a0d01b154513ca7f9c5d43c536b8
panyakorn <punyakorn314@gmail.com>
2026-09-18 18:13:40 +0700

    fix menu

--- backend/class.menu.php
+++ backend/class.menu.php
@@ -1,194 +1,49 @@
-<?php
-
-/*
- * Session ต้องเริ่มก่อนอ่าน $_SESSION
- */
-if (session_status() === PHP_SESSION_NONE)
-{
-    session_start();
-}
-
-require_once('../config/class.connect.php');
-
-?>
-
-
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-
     <div class="container-fluid">
-
-        <a
-            class="navbar-brand"
-            href="../frontend/home.php"
-        >
-            Project
-        </a>
-
-
-        <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-        >
-
-            <span class="navbar-toggler-icon"></span>
-
+        <a class="navbar-brand" href="../frontend/home.php">Project</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
         </button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-
-        <div
-            class="collapse navbar-collapse"
-            id="navbarSupportedContent"
-        >
-
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-
-<?php
-
-/*
- * ตรวจสอบว่า Login แล้วหรือยัง
- */
-if (
-    isset($_SESSION['uid']) &&
-    $_SESSION['uid'] != ''
-)
-{
-
-    $uid = (int)$_SESSION['uid'];
-
-
-    /*
-     * ดึง Menu ตาม User
-     */
-    $sql = "
-        SELECT
-            app.id,
-            app.name,
-            app.dir,
-            app.detail,
-            app.appgroup,
-            MAX(acl.accl) AS accl
-
-        FROM app
-
-        INNER JOIN acl
-            ON acl.appid = app.id
-
-        INNER JOIN uig
-            ON uig.ugid = acl.ugid
-
-        WHERE
-            app.status = 1
-            AND acl.status = 1
-            AND uig.status = 1
-            AND uig.uid = ?
-            AND acl.accl > 0
-
-        GROUP BY
-            app.id,
-            app.name,
-            app.dir,
-            app.detail,
-            app.appgroup
-
-        ORDER BY
-            app.appgroup ASC,
-            app.id ASC
-    ";
-
-
-    $conn = new connect();
-
-    $res = $conn->query(
-        $sql,
-        array($uid)
-    );
-
-
-    while ($cdr = $res->fetch())
-    {
-
-        echo '<li class="nav-item">';
-
-        echo '<a
-                class="nav-link"
-                href="index.php?option='
-                . htmlspecialchars($cdr['dir'], ENT_QUOTES, 'UTF-8')
-                . '&task=def"
-              >';
-
-        echo htmlspecialchars(
-            $cdr['name'],
-            ENT_QUOTES,
-            'UTF-8'
-        );
-
-        echo '</a>';
-
-        echo '</li>';
-    }
-
-?>
-
-
-                <li class="nav-item">
-
-                    <a
-                        class="nav-link"
-                        href="../frontend/home.php"
-                    >
-                        Homepage
-                    </a>
-
-                </li>
-
-
-                <li class="nav-item">
-
-                    <a
-                        class="nav-link"
-                        href="index.php?option=logs&task=logout"
-                    >
-                        Log out
-                    </a>
-
-                </li>
-
-
-<?php
-
-}
-else
-{
-
-?>
-
-
-                <li class="nav-item">
-
-                    <a
-                        class="nav-link"
-                        href="index.php?option=logs&task=login_form"
-                    >
-                        Log in
-                    </a>
-
-                </li>
-
-
-<?php
-
-}
-
-?>
-
-
-            </ul>
-
+			<?php
+			require_once('../config/class.connect.php');
+			if (isset($_SESSION['uid']))
+			{
+				$sql = "select * from `app`,`acl`,`uig` 
+				where `app`.`status` = '1' and `acl`.`status` = '1' and `uig`.`status` = '1' and `acl`.`appid` = `app`.`id` and `acl`.`ugid` = `uig`.`ugid` and `uig`.`uid` = '".$_SESSION['uid']."' 
+				order by `app`.`appgroup`";
+				$conn = new connect();
+				$res = $conn->query($sql);
+				while ($cdr = $res->fetch())
+				{
+					echo '<li class="nav-item">';
+					echo '<a class="nav-link" href="index.php?option='.$cdr['dir'].'&task=def">';
+					echo $cdr['name'];
+					echo '</a>';
+					echo '</li>';
+					echo'</li>';
+				}		
+			?>
+				<li class="nav-item">
+					<a class="nav-link" aria-current="page" href="../frontend/home.php">Homepage</a>
+				</li>
+				<li class="nav-item">
+				<a class="nav-link" aria-current="page" href="index.php?option=logs&task=logout">Log out</a>
+				</li>
+			<?php
+			}
+			else 
+			{
+			?>
+				<li class="nav-item">
+				<a class="nav-link" aria-current="page" href="index.php?option=logs&task=login_form">Log in</a>
+				</li>
+			<?php
+			}
+		?>
+        </ul>
         </div>
-
     </div>
-
 </nav>
